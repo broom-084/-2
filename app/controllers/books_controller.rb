9d1ad36145609d1a89aca0c6b3afaf0d53cec02c
EC2 Default User <ec2-user@ip-172-31-24-132.ap-northeast-1.compute.internal>
2023-11-13 10:14:07 +0000

    課題2の更新

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,7 +3,7 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to '/show'
+    redirect_to book_path(book.id)
   end
 
   def index
@@ -22,7 +22,7 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     book.update(book_params)
-    redirect_to '/show'
+    redirect_to book_path(book.id)
   end
   
   private
