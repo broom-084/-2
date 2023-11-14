afb522ee6b6b5d9ab92f0c94789b1b1dfa85a420
EC2 Default User <ec2-user@ip-172-31-24-132.ap-northeast-1.compute.internal>
2023-11-14 15:44:50 +0000

    フラッシュメッセージエラー発生中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,14 @@
 class BooksController < ApplicationController
   
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      flash.now[:error_explanation] = "失敗"
+      render :index
+    end
   end
 
   def index
