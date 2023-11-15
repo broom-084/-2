f424da5a67d45c4517a9f62ad7a6b2945853026f
EC2 Default User <ec2-user@ip-172-31-24-132.ap-northeast-1.compute.internal>
2023-11-15 14:04:16 +0000

    エラー表示以外完成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,11 +2,11 @@ class BooksController < ApplicationController
   
   def create
     @book = Book.new(book_params)
+    @books = Book.all
     if @book.save
       flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
-      flash.now[:error_explanation] = "失敗"
       render :index
     end
   end
@@ -25,14 +25,19 @@ class BooksController < ApplicationController
   end
   
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
   
   def destroy
-    book = Book.find(params[:id])
-    book.destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    flash[:notice] = "Book was successfully destroyed."
     redirect_to books_path
   end
   
